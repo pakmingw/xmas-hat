0d8212a7be3d38306626435261f09d2360b164e8
Pak Ming Wan <pakmingw@gmail.com>
2020-12-02 00:21:16 +0100

    adding constant

--- xmas-hat-1.py
+++ xmas-hat-1.py
@@ -8,6 +8,10 @@ import rainbowhat
 print("Rainbow HAT Christmas Edition:  Press Control+C to quit.")
 
 # Merry Christmas
+
+# constants
+BRIGHTNESS_VAL = 0.3
+
 rainbowhat.display.print_str("XMAS")
 rainbowhat.display.show()
 isFlip = True
@@ -16,14 +20,14 @@ while True:
         # red 
         if isFlip: 
             if x % 2 == 1:
-                rainbowhat.rainbow.set_pixel(6 - x, 0, 1, 0, brightness=0.1)
+                rainbowhat.rainbow.set_pixel(6 - x, 0, 1, 0, brightness=BRIGHTNESS_VAL)
             else: 
-                rainbowhat.rainbow.set_pixel(6 - x, 1, 0, 0, brightness=0.1)
+                rainbowhat.rainbow.set_pixel(6 - x, 1, 0, 0, brightness=BRIGHTNESS_VAL)
         else:
             if x % 2 == 0:
-                rainbowhat.rainbow.set_pixel(6 - x, 0, 1, 0, brightness=0.2)
+                rainbowhat.rainbow.set_pixel(6 - x, 0, 1, 0, brightness=BRIGHTNESS_VAL)
             else: 
-                rainbowhat.rainbow.set_pixel(6 - x, 1, 0, 0, brightness=0.2)
+                rainbowhat.rainbow.set_pixel(6 - x, 1, 0, 0, brightness=BRIGHTNESS_VAL)
     rainbowhat.rainbow.show()
     time.sleep(1)
     isFlip = not(isFlip)
