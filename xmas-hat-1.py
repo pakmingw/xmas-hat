7c17c9cd1907f1500406a3ec6506ca1b06f80557
Pak Ming Wan <pakmingw@gmail.com>
2020-12-02 00:08:17 +0100

    update color

--- xmas-hat-1.py
+++ xmas-hat-1.py
@@ -12,11 +12,18 @@ rainbowhat.display.print_str("XMAS")
 rainbowhat.display.show()
 
 while True:
+    isFlip = True
     for x in range(7):
         # red 
-        if x % 2 == 1:
-            rainbowhat.rainbow.set_pixel(6 - x, 1, 0, 0, brightness=0.1)
-        else: 
-            rainbowhat.rainbow.set_pixel(6 - x, 0, 1, 0, brightness=0.1)
+        if isFlip: 
+            if x % 2 == 1:
+                rainbowhat.rainbow.set_pixel(6 - x, 0, 1, 0, brightness=0.1)
+            else: 
+                rainbowhat.rainbow.set_pixel(6 - x, 1, 0, 0, brightness=0.1)
+        else:
+                if x % 2 == 1:
+                rainbowhat.rainbow.set_pixel(6 - x, 0, 1, 0, brightness=0.2)
+            else: 
+                rainbowhat.rainbow.set_pixel(6 - x, 1, 0, 0, brightness=0.2)
     rainbowhat.rainbow.show()
     time.sleep(1)
