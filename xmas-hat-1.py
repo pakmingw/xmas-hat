365aa081db77451f605553f975061d2fd93e1300
Pak Ming Wan <pakmingw@gmail.com>
2020-12-01 23:52:42 +0100

    initial push

---
+++ xmas-hat-1.py
@@ -0,0 +1,20 @@
+
+#!/usr/bin/env python
+
+import time
+
+import rainbowhat
+
+print("""Rainbow HAT Christmas Edition
+
+Press Control+C to quit.
+""")
+
+# Merry Christmas
+
+while True:
+
+    rainbowhat.display.print_str("XMAS")
+    rainbowhat.display.show()
+    time.sleep(1.0)
+
